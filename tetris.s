2aa38238774ad96954cfc86883b1aecab04b6e90
Emma Joe Anderson <jandew+dev@gmail.com>
2019-04-28 12:33:45 -0500

    include personal high scores, finish documentation

--- tetris.s
+++ tetris.s
@@ -156,28 +156,28 @@ clearDrought:
 .segment "DEFAULT_HIGH_SCORES"
 
 ; replace defaultHighScoresTable
-; # Convert ALPHA string to hex:
-; list(map(lambda x: hex(ord(x)-64), "CHEATR"))
         ; NAME
-        .byte   $0C,$01,$0E,$03,$05,$2B ; "LANCE "
-        .byte   $09,$13,$2B,$01,$2B,$2B ; "IS A  "
-        .byte   $03,$08,$05,$01,$14,$12 ; "CHEATR"
+        ; # Convert ALPHA string to hex:
+        ; list(map(lambda x: hex(ord(x)-64), "CHEATR"))
+        .byte   $01,$13,$08,$01,$12,$01 ; "ASHARA"
+        .byte   $05,$0D,$2A,$0A,$2B,$2B ; "EM.J  "
+        .byte   $05,$12,$09,$03,$2B,$2B ; "ERIC  "
         .byte   $00,$00,$00,$00,$00,$00
         .byte   $01,$0C,$05,$18,$2B,$2B
         .byte   $14,$0F,$0E,$19,$2B,$2B
         .byte   $0E,$09,$0E,$14,$05,$0E
         .byte   $00,$00,$00,$00,$00,$00
-        ; SCORE
-        .byte   $01,$00,$00
-        .byte   $00,$75,$00
-        .byte   $00,$50,$00
+        ; SCORE, in BCD
+        .byte   $39,$97,$72
+        .byte   $29,$51,$88
+        .byte   $17,$30,$68
         .byte   $00,$00,$00
         .byte   $00,$20,$00
         .byte   $00,$10,$00
         .byte   $00,$05,$00
         .byte   $00,$00,$00
-        ; LV
-        .byte   $09,$05,$00,$00
+        ; LV, in binary
+        .byte   $13,$12,$09,$00
         .byte   $09,$05,$00,$00
         .byte   $FF
 
