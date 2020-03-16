c1246b3bc0dbc1955906ed77b51b399493df1a48
Eric Anderson <ejona86@gmail.com>
2020-03-15 23:26:17 -1000

    handicap: Make more compatible with twoplayer
    Hasn't actually been tested with twoplayer, but made the IPS hunks
    non-conflicting and added player2 logic. Unfortunately, it is 5 bytes
    too large to fit in the leftover space of
    height_menu_nametablepalette_patch.

--- handicap.s
+++ handicap.s
@@ -5,13 +5,6 @@
 .include "build/tetris.inc"
 .include "ips.inc"
 
-.segment "CODEHDR"
-        ips_hunkhdr     "CODE"
-
-.segment "CODE"
-
-        .byte   $FF
-
 .segment "TYPE_A_MENU_PATCHHDR"
         ips_hunkhdr     "TYPE_A_MENU_PATCH"
 
@@ -22,51 +15,69 @@
         .byte   $20,$6D,$01,$0A
         .byte   $FF
 
-playfieldSize := $0003
+playfieldSize := $0043
+player1_playfieldSize := $0063
+player2_playfieldSize := $0084
 
 initGameState_mod:
-        jsr     memset_page
+        jsr     chooseNextTetrimino     ; replaced code
+
+        jsr     makePlayer1Active
+        jsr     initPlayfieldSize
+        jsr     savePlayer1State
+
+        jsr     makePlayer2Active
+        jsr     initPlayfieldSize
+        jmp     savePlayer2State
+
+initPlayfieldSize:
         lda     gameType
         bne     @typeB
-        ldy     startHeight
-        lda     heightToSizeTable,y
-        sta     playfieldSize
-        ldx     typeBBlankInitCountByHeightTable,y
+
+        ldx     startHeight
+        ldy     typeBBlankInitCountByHeightTable,x
         lda     #$4F
 @setRows:
-        cpx     #200
+        cpy     #200
         beq     @ret
-        sta     playfield,x
-        inx
+        sta     (playfieldAddr),y
+        iny
         bne     @setRows
+
 @typeB:
-        lda     #$16
-        sta     playfieldSize
+        ldx     #$00
 @ret:
+        lda     heightToSizeTable,x
+        sta     playfieldSize
         rts
+
 
 heightToSizeTable:
         .byte   20+2, 17+2, 15+2, 12+2, 10+2, 8+2
 
+.segment "UNREFERENCED_DATA3HDR"
+        ips_hunkhdr     "UNREFERENCED_DATA3"
+
+.segment "UNREFERENCED_DATA3"
+
 checkForCompletedRows_mod:
-        lda     tetriminoY
-        clc
-        adc     lineIndex
+        tay     ; replaced code
+        lda     generalCounter2
+        ; clc performed recently
+        adc     #$02
         cmp     playfieldSize
-        bpl     @skip
-        ; run replaced code
-        lda     tetriminoY
-        sec
-        jmp     afterCheckForCompletedRowsMod
-@skip:
-        jmp     $9ACC   ; @rowNotComplete
+        bmi     @noskip
+        ldy     #$FF    ; (playfieldAddr),#$FF is always $EF
+@noskip:
+        ldx     #$0A    ; replaced code
+        rts
 
 .segment "JMP_INIT_GAME_STATEHDR"
         ips_hunkhdr     "JMP_INIT_GAME_STATE"
 
 .segment "JMP_INIT_GAME_STATE"
 
-; at beginning of initGameState, replaces "jsr memset_page"
+; within initGameState, replaces "jsr chooseNextTetrimino" after "sta player2_autorepeatY"
         jsr     initGameState_mod
 
 .segment "JMP_CHECK_FOR_COMPLETED_ROWSHDR"
@@ -74,9 +85,8 @@ checkForCompletedRows_mod:
 
 .segment "JMP_CHECK_FOR_COMPLETED_ROWS"
 
-; at @updatePlayfieldComplete in playState_checkForCompletedRows, replaces "lda tetriminoY; sec"
-        jmp     checkForCompletedRows_mod
-afterCheckForCompletedRowsMod:
+; at @yInRange in playState_checkForCompletedRows, replaces "tay; ldx #$0A"
+        jsr     checkForCompletedRows_mod
 
 .segment "ENABLE_HEIGHT_IN_TYPE_AHDR"
         ips_hunkhdr     "ENABLE_HEIGHT_IN_TYPE_A"
