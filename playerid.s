4c3462f01cea35572376c2b45b998adce1aeac40
Eric Anderson <ejona86@gmail.com>
2020-04-05 17:01:47 -0700

    playerid: Show id in Type B; remove sprite variant
    Having both sprite and nametable codepaths wasn't all that useful.
    Seems I had settled on nametable.
    
    Fixing Type B also fixed showing the id in twoplayer.

--- playerid.s
+++ playerid.s
@@ -15,41 +15,21 @@
         jsr     level_menu_check_select_pressed
         nop
 
-.segment "JMP_GAME_SHOW_PLAYERID_SPRITEHDR"
-        ips_hunkhdr     "JMP_GAME_SHOW_PLAYERID_SPRITE"
-
-.segment "JMP_GAME_SHOW_PLAYERID_SPRITE"
-
-.ifdef PLAYERID_SPRITE
-; in branchOnGameMode; replaces "jsr stageSpriteForNextPiece"
-        jsr     game_show_playerid_sprite
-.else
-        jsr     stageSpriteForNextPiece
-.endif
-
 .segment "JMP_GAME_SHOW_PLAYERID_BGHDR"
         ips_hunkhdr     "JMP_GAME_SHOW_PLAYERID_BG"
 
 .segment "JMP_GAME_SHOW_PLAYERID_BG"
 
-.ifndef PLAYERID_SPRITE
-; in initGameBackground; replaces "jsr twoDigsToPPU"
+; in initGameBackground; replaces "jsr waitForVBlankAndEnableNmi"
         jsr     game_show_playerid_game_mod
-.else
-        jsr     twoDigsToPPU
-.endif
 
 .segment "JMP_MENU_SHOW_PLAYERID_BGHDR"
         ips_hunkhdr     "JMP_MENU_SHOW_PLAYERID_BG"
 
 .segment "JMP_MENU_SHOW_PLAYERID_BG"
 
-.ifndef PLAYERID_SPRITE
 ; in render_mode_menu_screens; replaces "sta PPUSCROLL"
         jsr     game_show_playerid_menu_mod
-.else
-        sta     PPUSCROLL
-.endif
 
 .segment "CODEHDR"
         ips_hunkhdr     "CODE"
@@ -67,46 +47,11 @@ level_menu_check_select_pressed:
         and     #$07
         sta     playerId
 @render:
-.ifdef PLAYERID_SPRITE
-        jsr     stagePlayerIdSprite
-.endif
 
         ; replaced code
         lda     newlyPressedButtons
         cmp     #$01
         rts
-
-.ifdef PLAYERID_SPRITE
-game_show_playerid_sprite:
-        jsr     stageSpriteForNextPiece
-stagePlayerIdSprite:
-        lda     playerId
-        beq     @ret
-        ldx     oamStagingLength
-        lda     #$07
-        sta     oamStaging,x
-        inx
-        lda     playerId
-        sta     oamStaging,x
-        inx
-        lda     gameMode
-        cmp     #$03
-        beq     @menu
-        lda     #$03
-        jmp     @control
-@menu:
-        lda     #$00
-@control:
-        sta     oamStaging,x
-        inx
-        lda     #$F8
-        sta     oamStaging,x
-        inx
-        stx     oamStagingLength
-@ret:
-        rts
-
-.else
 
 game_show_playerid_menu_mod:
         sta     PPUSCROLL
@@ -114,11 +59,14 @@ game_show_playerid_menu_mod:
         cmp     #$03    ; level menu
         beq     game_show_playerid_bg
         rts
+
 game_show_playerid_game_mod:
-        jsr     twoDigsToPPU
         lda     playerId
-        bne     game_show_playerid_bg
-        rts
+        beq     @done
+        jsr     game_show_playerid_bg
+@done:
+        jmp     waitForVBlankAndEnableNmi       ; replaced code
+
 game_show_playerid_bg:
         lda     #$20
         sta     PPUADDR
@@ -126,8 +74,7 @@ game_show_playerid_bg:
         sta     PPUADDR
         lda     playerId
         bne     @display
-        lda     #$82
+        lda     #$82    ; hard-coded original select screen tile
 @display:
         sta     PPUDATA
         rts
-.endif
