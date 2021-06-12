383e70f83eb8142b109538d113f04c3ce25792d7
Eric Anderson <ejona86@gmail.com>
2021-06-12 09:26:04 -0500

    save-highscores: Check for uninitialized SRAM; use default high scores
    High scores are now initialized to the default high scores instead of
    zeros. The magic is moved to the SRAM to know if we've already
    initialized SRAM, and we memset all of SRAM to zero in case we want to
    save something else later without clearing high scores.
    
    Instead of placing our hack's code in defaultHighScoreTable, it is now
    in the cold boot detection and logic.
    
    Fixes #25

--- save-highscores.s
+++ save-highscores.s
@@ -1,7 +1,13 @@
+;
+; Enable SRAM and save highscores
+;
+
 .include "build/tetris.inc"
 .include "ips.inc"
 
 SRAM := $6000
+sramMagic := SRAM
+sramHighScoreTable := SRAM+5
 
 ; header patch to enable SRAM
 
@@ -18,36 +24,71 @@ INES_SRAM   = 1 ; 1 = battery backed SRAM at $6000-7FFF
 
         .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
 
-; patch to load from SRAM (and use a length instead of a terminator)
-
-.segment "initHighScoreTable_patch"
-        ; Address happens to be same for PAL
-        ips_segment     "initHighScoreTable_patch",$8095
-
-; initHighScoreTable_patch:
-initHighScoreTable:
-        lda SRAM,x
-        cpx #$50
-
-; start button patch to add saving
-
 .segment "saveHighscoreStartPressed_patch"
         ips_segment     "saveHighscoreStartPressed_patch",highScoreEntryScreen+$94 ; $A295
+
+; Save high scores after player presses start after entering name
 
 ; saveHighscoreStartPressed_patch:
         jmp saveHighScores
 
-; routine for storing the high score data
+.segment "initAndSave"
+        ips_segment     "initAndSave",initRamContinued+$13,$4F ; $806D
 
-.segment "saveHighScores"
-        ips_segment     "saveHighScores",defaultHighScoresTable
+continueWarmBootInit := initRamContinued+$62 ; $80BC
 
+; Redefine cold boot to mean uninitialized SRAM
+checkInit:
+        ldx #$05
+@checkColdBoot:
+        dex
+        bmi @warmInit
+        lda magicValue,x
+        cmp sramMagic,x
+        beq @checkColdBoot
+
+; Copy default high scores and magic to SRAM
+@coldInit:
+        ; Init all of SRAM, in case we use more in the future
+        lda #$00
+        ldx #>SRAM
+        ldy #$7F
+        jsr memset_page
+
+        ldx #$50-1
+@initSramHighScore:
+        lda defaultHighScoresTable,x
+        sta sramHighScoreTable,x
+        dex
+        bpl @initSramHighScore
+
+        ldx #$05-1
+@initSramMagic:
+        lda magicValue,x
+        sta sramMagic,x
+        dex
+        bpl @initSramMagic
+
+; Copy scores from SRAM every boot; initMagic unused
+@warmInit:
+        ldx #$50-1
+@loadHighScore:
+        lda sramHighScoreTable,x
+        sta highScoreNames,x
+        dex
+        bpl @loadHighScore
+        jmp continueWarmBootInit
+
+magicValue:
+        .byte $12,$34,$56,$78,$9A
+
+; Copy updated scores to SRAM
 saveHighScores:
-        ldx #$00
+        ldx #$50-1
 @loop:
         lda highScoreNames,x
-        sta SRAM,x
-        inx
-        cpx #$51
-        bne @loop
+        sta sramHighScoreTable,x
+        dex
+        bpl @loop
         jmp highScorePosToY-4 ; @ret := $A337
+
