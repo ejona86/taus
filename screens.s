ejona86/taus

commit a8d69940edfce398d5eeee3a320c416b59c2d709

tetris-PRG.info: Add local labels for bulkCopyToPpu

screens.s
@@ -108,7 +108,7 @@ my_bulkCopyToPpu:
         sta     tmp1
         lda     palettes+1,x
         sta     tmp2
-        jsr     copyToPpu
+        jsr     bulkCopyToPpu+2
         rts
 
 screens = 10

taus.s
@@ -345,7 +345,7 @@ renderStats:
         sta     tmp2
         lda     copyToPpuDuringRenderAddr
         sta     tmp1
-        jsr     copyToPpu
+        jsr     bulkCopyToPpu+2
         lda     #$00
         sta     copyToPpuDuringRenderAddr
         sta     copyToPpuDuringRenderAddr+1

tetris-PRG.info
@@ -964,9 +964,18 @@ LABEL { ADDR $AA6B; NAME "waitForVBlankAndEnableNmi"; };
 LABEL { ADDR $AA78; NAME "disableNmi"; };
 LABEL { ADDR $AA7C; NAME "_updatePpuCtrl"; };
 LABEL { ADDR $AA8A; NAME "copyCurrentScrollAndCtrlToPPU"; };
-LABEL { ADDR $AA98; NAME "bulkCopyToPpu"; };
-LABEL { ADDR $AAF2; NAME "copyToPpu"; COMMENT "Address to read from stored in tmp1/2"; };
-LABEL { ADDR $AB21; NAME "copyAddrAtReturnAddressToTmp_incrReturnAddrBy2"; };
+LABEL { ADDR $AA98; NAME "bulkCopyToPpu"; COMMENT "Return address should contain data address; ret addr+2 is actual return. $80-$FF end. $4C recurse, 2 byte cpu addr. $60 unrecurse. Otherwise NES data stripe, 2 byte ppu addr, 1 byte length, data. length controls RLE and write direction"; };
+LABEL { ADDR $AA9E; NAME "@processStripe"; };
+LABEL { ADDR $AAB5; NAME "@setDirection"; COMMENT "Bit 7 of length: 0-right, 1-down"; };
+LABEL { ADDR $AAC2; NAME "@checkForZeroLength"; COMMENT "Bit 6 of length: 0-literal, 1-run. If bits 0-5 are zero, length is 128"; };
+LABEL { ADDR $AAC7; NAME "@clearLengthFlagBits"; };
+LABEL { ADDR $AACA; NAME "@copyByteLoop"; };
+LABEL { ADDR $AACD; NAME "@copyByte"; };
+LABEL { ADDR $AAE6; NAME "@pointToNext"; };
+LABEL { ADDR $AAF2; NAME "@processInst"; COMMENT "Address to read from stored in tmp1/2"; };
+LABEL { ADDR $AAFC; NAME "@checkUnrecurse"; };
+LABEL { ADDR $AB0A; NAME "@checkRecurse"; };
+LABEL { ADDR $AB21; NAME "bulkCopyToPpu_reworkReturnAddress"; COMMENT "Copies address at return to tmp0/tmp1. Increments return by two"; };
 LABEL { ADDR $AB47; NAME "generateNextPseudorandomNumber"; COMMENT "reg x: zeropage addr of seed; reg y: size of seed"; }; # canon
 LABEL { ADDR $AB57; NAME "@updateNextByteInSeed"; };
 LABEL { ADDR $AB5E; NAME "copyOamStagingToOam"; COMMENT "canon is initializeOAM"; };

twoplayer.s
@@ -161,7 +161,7 @@ twoplayer_game_nametable:
 
 copyRleNametableToPpu:
         .export copyRleNametableToPpu
-        jsr     copyAddrAtReturnAddressToTmp_incrReturnAddrBy2
+        jsr     bulkCopyToPpu_reworkReturnAddress
         ldx     PPUSTATUS
         lda     #$20
         sta     PPUADDR

commit 685d63c958ebafe25b07c814a4836fddc7e9fb34

Fix bulkCopyToPpu offset for copyToPpu
The wrong offset was introduced in a8d69940

screens.s
@@ -108,7 +108,7 @@ my_bulkCopyToPpu:
         sta     tmp1
         lda     palettes+1,x
         sta     tmp2
-        jsr     bulkCopyToPpu+2
+        jsr     bulkCopyToPpu+3
         rts
 
 screens = 10

taus.s
@@ -345,7 +345,7 @@ renderStats:
         sta     tmp2
         lda     copyToPpuDuringRenderAddr
         sta     tmp1
-        jsr     bulkCopyToPpu+2
+        jsr     bulkCopyToPpu+3
         lda     #$00
         sta     copyToPpuDuringRenderAddr
         sta     copyToPpuDuringRenderAddr+1